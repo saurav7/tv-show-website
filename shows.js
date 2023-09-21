7537f4c601d994c255c76b97388feaf2d6e3d0d6
Saurav <saurav.ronaldo7@gmail.com>
2023-09-22 07:50:03 +1000

    append sibling and child to parent div

--- shows.js
+++ shows.js
@@ -2,14 +2,54 @@ const main = document.querySelector('main')
 
 // Making API Call
 async function getTvShows() {
-    try{
+    try {
         const res = await axios.get("https://api.tvmaze.com/shows")
-    const results = res.data
-    showImages(results)
-    }catch(err){
+        const results = res.data
+        showImages(results)
+    } catch (err) {
         console.error(err)
     }
-    
 }
 
 getTvShows()
+
+
+
+// Render Shows
+function showImages(shows) {
+    main.innerHTML = ''
+
+    shows.forEach((show) => {
+
+        // Show card with image, title, seeInfo button
+        const showCard = document.createElement('div')
+        showCard.classList.add('show-card')
+
+       
+        // Div that includes show image, title, button so that we can use flex
+        const showImgTitleButton = document.createElement('div')
+        showImgTitleButton.classList.add('show-img-title-button')
+
+        
+        // Show Image
+        const img = document.createElement('img')
+        img.classList.add('card-img')
+        img.src = show.image.medium
+
+        
+        //  Show Title 
+        const title = document.createElement('h5')
+        title.innerText = show.name
+
+        
+        // Show SeeInfo Button
+        const seeInfo = document.createElement('button')
+        seeInfo.classList.add('show-info')
+        seeInfo.innerText = 'See Info'
+
+
+        showImgTitleButton.append(img, title, seeInfo)
+        showCard.append(showImgTitleButton)
+        main.append(showCard)
+    })
+}
