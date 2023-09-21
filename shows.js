427bbb72b35a84943888772430424972c608a526
Saurav <saurav.ronaldo7@gmail.com>
2023-09-22 07:51:38 +1000

    little update in show.js after appending

--- shows.js
+++ shows.js
@@ -25,23 +25,23 @@ function showImages(shows) {
         const showCard = document.createElement('div')
         showCard.classList.add('show-card')
 
-       
+
         // Div that includes show image, title, button so that we can use flex
         const showImgTitleButton = document.createElement('div')
         showImgTitleButton.classList.add('show-img-title-button')
 
-        
+
         // Show Image
         const img = document.createElement('img')
         img.classList.add('card-img')
         img.src = show.image.medium
 
-        
+
         //  Show Title 
         const title = document.createElement('h5')
         title.innerText = show.name
 
-        
+
         // Show SeeInfo Button
         const seeInfo = document.createElement('button')
         seeInfo.classList.add('show-info')
