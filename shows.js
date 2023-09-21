9d761acea3134ec8f54efb188acdab45ff87b3cc
Saurav <saurav.ronaldo7@gmail.com>
2023-09-22 07:39:56 +1000

    make api call in shows.js

---
+++ shows.js
@@ -0,0 +1,13 @@
+const main = document.querySelector('main')
+
+// Making API Call
+async function getTvShows() {
+    try{
+        const res = await axios.get("https://api.tvmaze.com/shows")
+    const results = res.data
+    showImages(results)
+    }catch(err){
+        console.error(err)
+    }
+    
+}
