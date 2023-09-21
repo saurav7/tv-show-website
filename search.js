424e533076d7807196949465f324683f3e0e8912
Saurav <saurav.ronaldo7@gmail.com>
2023-09-22 09:45:48 +1000

    create a function to render search shows card

--- search.js
+++ search.js
@@ -12,3 +12,34 @@ form.addEventListener('submit', async function (e) {
         console.error(err)
     }
 })
+
+// Render Searched Shows
+function showSearchImages(shows){
+    main.innerHTML = ''
+
+    for (let result of shows) {
+        if (result.show.image) {
+            // Show card with image, title, seeInfo button
+            const showCard = document.createElement('div')
+            showCard.classList.add('show-card')
+
+            // Div that includes show image, title, button so that we can use flex
+            const showImgTitleButton = document.createElement('div')
+            showImgTitleButton.classList.add('show-img-title-button')
+
+            // Show Image
+            const img = document.createElement('img')
+            img.classList.add('card-img')
+            img.src = result.show.image.medium
+
+            //  Show Title 
+            const title = document.createElement('h5')
+            title.innerText = result.show.name
+
+            // Show SeeInfo Button
+            const seeInfo = document.createElement('button')
+            seeInfo.classList.add('show-info')
+            seeInfo.innerText = 'See Info'
+        }
+    }
+}
